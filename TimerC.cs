0b8f2779103022ff96080515766e608629b44f33
wcharlesknight <williamchknight@gmail.com>
2021-03-18 18:32:51 -0700

    timer coming along - increased understanding of C#/Unity engine

---
+++ TimerC.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+using System;
+using System.Timers;
+using TMPro;
+
+public class TimerC : MonoBehaviour
+{   
+    public GameObject words; 
+    private System.Timers.Timer GameTimer;
+    int nEventsFired = 0; 
+    
+    void Start()
+    {   
+        GameTimer = new System.Timers.Timer();
+        GameTimer.Interval = 1000; 
+        GameTimer.Start();
+        GameTimer.Elapsed += AddTime;
+  
+    }
+
+    void Update()
+    {   
+        gameObject.GetComponent<TMP_Text>().text = Convert.ToString(nEventsFired, 10);
+    }
+    
+    public void AddTime(object source, ElapsedEventArgs e)
+    {   
+        Debug.Log(nEventsFired);
+        nEventsFired++;
+        if (nEventsFired == 7)
+          {
+            nEventsFired = 0; 
+          }
+      
+    }
+}
--- WebRequest.cs
+++ WebRequest.cs
@@ -17,21 +17,26 @@ public class WebRequest : MonoBehaviour
       StartCoroutine(GetLetters());
     }
 
+    public delegate void WordSetter(JSONNode letter);
+
+    public void SetWord(JSONNode letters)
+    { 
+      List<object> MainLetters = new List<object>();
+      Text text = gameObject.GetComponent<Text>();
+      text.text = "";
+      foreach (JSONNode element in letters)
+      {
+        text.text += element["character"];
+      }
+    }
+
     IEnumerator GetLetters()
     {
       UnityWebRequest letters = UnityWebRequest.Get("http://localhost:3000/letters");
       yield return letters.SendWebRequest();
-    
-      JSONNode letterInfo = JSON.Parse(letters.downloadHandler.text);
-      Text text = gameObject.GetComponent<Text>();
-      
-      foreach (var element in letterInfo)
-      {
-      Debug.Log(element["character"]);
-      } 
+      JSONNode letterInfo = JSON.Parse(letters.downloadHandler.text); 
+      WordSetter del = SetWord;
+      del(letterInfo);
     }
 
-    public void FillWord(words)
-
-    
 }
